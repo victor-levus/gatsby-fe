010c9031ccfaf4a13325f646707f396736462fff
victorlevus <victorlevus@mail.ru>
2022-10-10 22:11:55 +0100

    add Image plugins to the app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="image goes here" src="../images/apartment.webp" />
     </Layout>
   );
 };
